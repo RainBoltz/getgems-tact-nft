be7a00709e60b0e976d70fb837eea6d08bab57b9
Philip <philip27020012@gmail.com>
2024-04-26 03:26:27 +0800

    feat: add mint test

--- tests/Nft.spec.ts
+++ tests/Nft.spec.ts
@@ -1,21 +1,36 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
-import { Nft } from '../wrappers/Nft';
+import { toNano, beginCell } from '@ton/core';
+import { NftCollectionWrap } from '../wrappers/Nft';
 import '@ton/test-utils';
 
 describe('Nft', () => {
+    const { NftCollection } = NftCollectionWrap;
+    const nftCollectionContentUrl =
+        'https://cloudflare-ipfs.com/ipfs/QmYvP3RsDpBQiNSxueZkgEhqsJA17oJk3vjkMFfN7ZvFTv?filename=collection_meta.json';
+    const nftItemContentUrl = 'https://d2126epqdbsmq2.cloudfront.net/info/';
+
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let nft: SandboxContract<Nft>;
+
+    // eslint-disable-next-line
+    let nftCollection: any;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        nft = blockchain.openContract(await Nft.fromInit(0n));
+        nftCollection = blockchain.openContract(
+            await NftCollection.fromInit(
+                0n,
+                beginCell().storeUint(1, 8).storeStringTail(nftCollectionContentUrl).endCell(),
+                beginCell().storeUint(1, 8).storeStringTail(nftItemContentUrl).endCell(),
+                0n,
+                deployer.address,
+            ),
+        );
 
         deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await nft.send(
+        const deployResult = await nftCollection.send(
             deployer.getSender(),
             {
                 value: toNano('0.05'),
@@ -23,12 +38,12 @@ describe('Nft', () => {
             {
                 $$type: 'Deploy',
                 queryId: 0n,
-            }
+            },
         );
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: nft.address,
+            to: nftCollection.address,
             deploy: true,
             success: true,
         });
@@ -39,44 +54,27 @@ describe('Nft', () => {
         // blockchain and nft are ready to use
     });
 
-    it('should increase counter', async () => {
-        const increaseTimes = 3;
-        for (let i = 0; i < increaseTimes; i++) {
-            console.log(`increase ${i + 1}/${increaseTimes}`);
+    it('should mint item', async () => {
+        const itemIndex = 0n;
 
-            const increaser = await blockchain.treasury('increaser' + i);
+        const mintResult = await nftCollection.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.15'),
+            },
+            {
+                $$type: 'NftCollectionDeployNewNft',
+                query_id: 0n,
+                item_index: itemIndex,
+                nft_content: beginCell().storeStringTail(`${itemIndex}`).endCell(),
+            },
+        );
 
-            const counterBefore = await nft.getCounter();
-
-            console.log('counter before increasing', counterBefore);
-
-            const increaseBy = BigInt(Math.floor(Math.random() * 100));
-
-            console.log('increasing by', increaseBy);
-
-            const increaseResult = await nft.send(
-                increaser.getSender(),
-                {
-                    value: toNano('0.05'),
-                },
-                {
-                    $$type: 'Add',
-                    queryId: 0n,
-                    amount: increaseBy,
-                }
-            );
-
-            expect(increaseResult.transactions).toHaveTransaction({
-                from: increaser.address,
-                to: nft.address,
-                success: true,
-            });
-
-            const counterAfter = await nft.getCounter();
-
-            console.log('counter after increasing', counterAfter);
-
-            expect(counterAfter).toBe(counterBefore + increaseBy);
-        }
+        expect(mintResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: nftCollection.address,
+            deploy: false,
+            success: true,
+        });
     });
 });
